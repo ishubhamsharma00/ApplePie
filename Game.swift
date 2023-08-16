4751178683caac04f621b538b7bb08ccfb9ae17f
Shubham Sharma <83246016+ishubhamsharma00@users.noreply.github.com>
2023-08-16 16:04:30 +0530

    Add files via upload

---
+++ Game.swift
@@ -0,0 +1,21 @@
+//
+//  Game.swift
+//  ApplePie
+//
+//  Created by RCAT4 on 16/08/23.
+//
+
+import Foundation
+struct Game {
+    var word: String
+    var incorrectMovesRemaining: Int
+    
+    var guessedLetters: [Character]
+    mutating func playerGuessed(letter: Character) {
+          guessedLetters.append(letter)
+          if !word.contains(letter) {
+            incorrectMovesRemaining -= 1
+          }
+        }
+}
+
